8a12f8592b7498fae6bc67d9e92f7379c470a7c8
hirabayashi-masashi <masashi.hirabayashi@di-v.co.jp>
2021-02-12 09:38:12 +0900

    create file and csv

---
+++ create_csv.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"encoding/csv"
+	"log"
+	"os"
+)
+
+var sample = []string{
+	"sample_1",
+	"sample_2",
+	"sample_3",
+}
+
+func createCsv() {
+	//書き込みファイル作成
+	file, err := os.Create("/出力したいパス/sample.csv")
+	if err != nil {
+		log.Println(err)
+	}
+	defer file.Close()
+
+	writer := csv.NewWriter(file) // utf8
+	writer.Write(sample)
+	writer.Flush() // ファイル出力
+}
